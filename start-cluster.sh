784dcd512aa33e58e4872177e8153694504f9d34
James Mills <prologic@shortcircuit.net.au>
2021-06-24 16:30:48 +1000

    Fix quick 'n dirty start cluster script

--- start-cluster.sh
+++ start-cluster.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+cleanup() {
+  pkill fbox
+}
+
 test -d data1 || mkdir -p data1
 test -d data2 || mkdir -p data2
 test -d data3 || mkdir -p data3
 
-nohup ./fbox -a 10.0.0.109:8001 -b 0.0.0.0:8001 -d ./data1 -m http://10.0.0.101:8000 > fbox.log.1 2>&1 &
-nohup ./fbox -a 10.0.0.109:8002 -b 0.0.0.0:8002 -d ./data2 -m http://10.0.0.101:8000 > fbox.log.2 2>&1 &
-nohup ./fbox -a 10.0.0.109:8003 -b 0.0.0.0:8003 -d ./data3 -m http://10.0.0.101:8000 > fbox.log.3 2>&1 &
+./fbox -a 10.0.0.104:8000 -b 0.0.0.0:8000 -d ./data1 > fbox.log.1 2>&1 &
+./fbox -a 10.0.0.104:8001 -b 0.0.0.0:8001 -d ./data2 -m http://10.0.0.104:8000 > fbox.log.2 2>&1 &
+./fbox -a 10.0.0.104:8002 -b 0.0.0.0:8002 -d ./data3 -m http://10.0.0.104:8000 > fbox.log.3 2>&1 &
+
+trap cleanup EXIT
 
 wait
