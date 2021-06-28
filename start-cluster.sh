4c8588ee3d16778d46a9ecc46887dc20f5e2edad
James Mills <prologic@shortcircuit.net.au>
2021-06-28 21:36:56 +1000

    Add IP variable to start-cluster.sh to reduce typing

--- start-cluster.sh
+++ start-cluster.sh
@@ -4,13 +4,15 @@ cleanup() {
   pkill fbox
 }
 
+IP="10.0.0.101"
+
 test -d data1 || mkdir -p data1
 test -d data2 || mkdir -p data2
 test -d data3 || mkdir -p data3
 
-./fbox -a 192.168.1.6:8000 -b 0.0.0.0:8000 -d ./data1 > fbox.log.1 2>&1 &
-./fbox -a 192.168.1.6:8001 -b 0.0.0.0:8001 -d ./data2 -m http://192.168.1.6:8000 > fbox.log.2 2>&1 &
-./fbox -a 192.168.1.6:8002 -b 0.0.0.0:8002 -d ./data3 -m http://192.168.1.6:8000 > fbox.log.3 2>&1 &
+./fbox -a "$IP:8000" -b 0.0.0.0:8000 -d ./data1 > fbox.log.1 2>&1 &
+./fbox -a "$IP:8001" -b 0.0.0.0:8001 -d ./data2 -m "http://$IP:8000" > fbox.log.2 2>&1 &
+./fbox -a "$IP:8002" -b 0.0.0.0:8002 -d ./data3 -m "http://$IP:8000" > fbox.log.3 2>&1 &
 
 trap cleanup EXIT
 
