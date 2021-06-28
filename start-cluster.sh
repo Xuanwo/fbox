0d00383211a09592a45fb9dfbb43ee2fbf67a5de
James Mills <prologic@shortcircuit.net.au>
2021-06-28 22:11:33 +1000

    Add a sleep between starting master and nodes

--- start-cluster.sh
+++ start-cluster.sh
@@ -11,6 +11,7 @@ test -d data2 || mkdir -p data2
 test -d data3 || mkdir -p data3
 
 ./fbox -a "$IP:8000" -b 0.0.0.0:8000 -d ./data1 > fbox.log.1 2>&1 &
+sleep 1
 ./fbox -a "$IP:8001" -b 0.0.0.0:8001 -d ./data2 -m "http://$IP:8000" > fbox.log.2 2>&1 &
 ./fbox -a "$IP:8002" -b 0.0.0.0:8002 -d ./data3 -m "http://$IP:8000" > fbox.log.3 2>&1 &
 
