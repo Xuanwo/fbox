c4c7e6aa8d7362ca0bcd92b0ae355dc95b56035e
Mark <me@markwylde.com>
2021-06-28 22:05:55 +1000

    Fix paths for ui branch

--- ui/js/actions/listFiles.js
+++ ui/js/actions/listFiles.js
@@ -1,5 +1,5 @@
 export default async function listFiles (context) {
-  const response = await fetch('/api/files');
+  const response = await fetch('/files');
   const json = await response.json();
 
   const files = Object
--- ui/js/actions/uploadFile.js
+++ ui/js/actions/uploadFile.js
@@ -1,5 +1,5 @@
 export default async function uploadFile (file) {
-  const response = await fetch('/api/upload/' + file.name, {
+  const response = await fetch('/upload/' + file.name, {
     method: 'post',
     body: file
   });
