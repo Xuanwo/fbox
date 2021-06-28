63190984644668b7775b2b295a4ab4c34ac842f7
Mark <me@markwylde.com>
2021-06-28 21:40:29 +1000

    Add multiple files in uploading

--- ui/js/components/FileUploader.js
+++ ui/js/components/FileUploader.js
@@ -14,6 +14,7 @@ export default function Header () {
         m('input', {
           id: 'file-uploader',
           onchange: handleChange(vnode.attrs.onChange),
+          multiple: 'multiple',
           type: 'file'
         })
       )
